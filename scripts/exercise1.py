67171b381d8460b9a959f559357387ad76585de1
deblina23 <66369685+deblina23@users.noreply.github.com>
2020-06-15 11:18:03 +0530

    Update exercise1.py
    extra number deletion

--- scripts/exercise1.py
+++ scripts/exercise1.py
@@ -4,5 +4,5 @@ print(" Write a Python program to print the following string in a specific forma
 print("Sample String : Twinkle, twinkle, little star, How I wonder what you are! Up above the world so high, Like a diamond in the sky. Twinkle, twinkle, little star, How I wonder what you are\n\n")
 print("Solution:\n")
 print("Twinkle, twinkle, little star,\n\t How I wonder what you are!\n\t\t Up above the world so high,\n\t\t Like a diamond in the sky. \nTwinkle, twinkle, little star,\n\t How I wonder what you are")
-8
+
 
