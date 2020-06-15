60f95ada71f3d840de22b8e44f0776385814a48f
deblina23 <66369685+deblina23@users.noreply.github.com>
2020-06-15 11:19:43 +0530

    Update exercise4.py
    extra number  and line deletion

--- scripts/exercise4.py
+++ scripts/exercise4.py
@@ -1,10 +1,7 @@
 #! /usr/bin/env python3
-
 from math import pi
 print("Problem:\n")
 print("Write a Python program which accepts the radius of a circle from the user and compute the area.\n")
 print("Solution:\n")
 x=float(input())
-
-9
 print(pi* x**2)
