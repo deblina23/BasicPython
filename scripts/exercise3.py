19e4a81419e3a31886df69e4fb28a59f4623142c
deblina23 <66369685+deblina23@users.noreply.github.com>
2020-06-15 11:19:15 +0530

    Update exercise3.py
    extra number  and line deletion

--- scripts/exercise3.py
+++ scripts/exercise3.py
@@ -1,9 +1,6 @@
 #! /usr/bin/env python3
-
 import datetime
 print("Problem:\n")
 print(" Write a Python program to display the current date and time.\n")
 print("Solution\n")
 print(datetime.datetime.now())
-8
-
